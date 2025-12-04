600176ff6febebcf7feb76ca3f5027c66daf367a
linzeguang <15817289730@163.com>
2025-12-04 22:49:43 +0800

    Update Preview.tsx

--- src/components/swap/Preview.tsx
+++ src/components/swap/Preview.tsx
@@ -3,6 +3,8 @@ import { Trade } from '@pippyswap/v2-sdk'
 import { Currency, Rounding, TradeType } from '@uniswap/sdk-core'
 import React from 'react'
 
+import { cn } from '@/lib/utils'
+
 import KeyValue from '../common/KeyValue'
 import { KanitText } from '../ui/Text'
 
@@ -19,7 +21,12 @@ const Preview: React.FC<{ trade: Trade<Currency, Currency, TradeType> }> = ({ tr
             <Trans>Price impact</Trans>
           </KanitText>
         }
-        valueNode={trade.priceImpact.toFixed(2) + '%'}
+        valueNode={
+          <KanitText variant={'tertiary'} className={cn('text-xs')}>
+            {trade.priceImpact.greaterThan(0) ? '+' : ''}
+            {trade.priceImpact.toFixed(2) + '%'}
+          </KanitText>
+        }
       />
     </div>
   )
